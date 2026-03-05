155bcb9cf4ac2e56b9e6b5979aebcd0f368871de
Momin Sheikh <momincse13@gmail.com>
2026-03-05 15:40:32 +0600

    Add Azure deploy

--- SharpStack-Backend/Blog.API/Program.cs
+++ SharpStack-Backend/Blog.API/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("https://sharpstackbd.onrender.com", "http://localhost:3000")
+        policy.WithOrigins("https://sharpstackbd.onrender.com", "http://localhost:5173")
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials();
